e005c252a40278fc95bd6a975b3032b73657affd
nuotsu <Mitchell@Mitchells-MacBook-Pro.local>
2017-03-24 17:44:04 -0700

    v1.3

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,8 @@ $(function() {
 		$('body').prepend('<div class="bg"></div>')
 		$('.bg').css({
 			'position':'fixed',
-			'left':'0',
-			'right': '0',
-			'top': '0',
-			'bottom': '0',
+			'left':'0',	'right': '0',
+			'top': '0',	'bottom': '0',
 			'z-index': '-1'
 		})
 
