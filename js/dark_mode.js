ba3a54c9d66e6ec9ac65ebaf2cf865b83aef69e3
nuotsu <Mitchell@Mitchells-MacBook-Pro.local>
2017-04-13 15:45:05 -0700

    v4

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -15,16 +15,18 @@ $(function() {
       $(this).toggleClass('LM_txt_ENG')
     }
 
-    // PPAR
-      $('body, .howto').toggleClass('darkest_bg')
-      $('.language a').toggleClass('language_txt')
-      $('.language a, .top, .settings, .settings input, .display input, .howto, #mode').toggleClass('white_txt')
-      $('#randomize, #organize, #locate, #showAll, #save').toggleClass('btn_color')
-      $('#randomize, #organize, #locate, #showAll, #save').toggleClass('dark_bg')
-    // Counter
-      $('.counter, .display input').toggleClass('darker_bg')
-      $('.c-1 .plus, .c-1 .minus, .c-2 .plus, .c-2 .minus').toggleClass('btn_color')
-      $('.buttons td').toggleClass('dark_bg')
-      $('.reset').toggleClass('light_bg')
+  // PPAR
+    $('body, .howto').toggleClass('darkest_bg')
+    $('.language a').toggleClass('language_txt')
+    $('.language a, .top, .settings, .settings input, .display input, .howto, #mode').toggleClass('white_txt')
+    $('#randomize, #organize, #locate, #showAll, #save').toggleClass('btn_color')
+    $('#randomize, #organize, #locate, #showAll, #save').toggleClass('dark_bg')
+    $('#result').toggleClass('white_border')
+
+  // Counter
+    $('.counter, .display input').toggleClass('darker_bg')
+    $('.c-1 .plus, .c-1 .minus, .c-2 .plus, .c-2 .minus').toggleClass('btn_color')
+    $('.buttons td').toggleClass('dark_bg')
+    $('.reset').toggleClass('light_bg')
   })
 })
--- js/ppar.js
+++ js/ppar.js
@@ -51,28 +51,76 @@ $(function() {
 			})
 		})
 
+	// Collection
+		$('#collection').change(function() {
+			if ($(this).val() == 'all') {
+				$('.wallpaper img').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'kanto') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=kanto]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'johto') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=johto]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'hoenn') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=hoenn]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'sinnoh') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=sinnoh]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'unova') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=unova]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'kalos') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=kalos]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'alola') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=alola]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'mega') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=mega]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'rf') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=rf]').css({'opacity': '1'})
+			}
+			if ($(this).val() == 'starter') {
+				$('.wallpaper img').css({'opacity': '0'})
+				$('.wallpaper img[data-collection*=starter]').css({'opacity': '1'})
+			}
+		})
+
 	// Locate
-		//Hide Rest Initial
+		// Hide Rest Initial
 			$('.hideRest').css({
 				'pointer-events': 'none',
 				'opacity': '0.3'
 			})
-		$('#locate').click(function() {
-			$('.wallpaper img').css({
-				'opacity': '0.1'
-			})
-			find = $('.find').val().split(',')
-			for (i = 0; i <= find.length; i++) {
-				$('.wallpaper img[src*=' + find[i] +']').css({
-					'opacity': '1',
-					// 'z-index': $('.wallpaper img').length
-			})
-			}
-			$('.hideRest').css({
-				'pointer-events': 'auto',
-				'opacity': '1'
-			})
-			$('#hideRest').prop('checked', false)
+		// Locate
+			$('#locate').click(function() {
+				$('.wallpaper img').css({
+					'opacity': '0.1'
+				})
+				find = $('.find').val().split(',')
+				for (i = 0; i <= find.length; i++) {
+					$('.wallpaper img[src*=' + find[i] +']').css({
+						'opacity': '1',
+						// 'z-index': $('.wallpaper img').length
+				})
+				}
+				$('.hideRest').css({
+					'pointer-events': 'auto',
+					'opacity': '1'
+				})
+				$('#hideRest').prop('checked', false)
 			// Hide Rest
 				$('#hideRest').change(function() {
 					if ($(this).prop('checked') == true) {
@@ -99,18 +147,26 @@ $(function() {
 						}
 					}
 				})
-		})
-	// Show All
-		$('#showAll').click(function() {
-			$('.wallpaper img').css({
-				'opacity': '1'
 			})
-		})
+			// Show All
+				$('#showAll').click(function() {
+					$('.wallpaper img').css({
+						'opacity': '1'
+					})
+				})
 
 	// Keypresses
 		$(document).keypress(function(e) {
 			current = $('.c-active #number').val()
 			keycode = (e.keycode ? e.keycode : e.which)
+		// 'r' = Randomize
+			if (keycode == '114') {
+				$('#randomize').click()
+			}
+		// 'o' = Organize
+			if (keycode == '111') {
+				$('#organize').click()
+			}
 		// 'd' = Dark Mode
 			if (keycode == '100') {
 				$('#mode').click()
